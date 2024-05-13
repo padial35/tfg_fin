58c7d35b6bd042260b9ec5f6ca0c0d5e9c645c95
padial35 <pablopadial7@hotmail.com>
2024-05-13 12:58:39 +0200

    simpleMain

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tfg/registro.dart';
 import 'package:tfg/inicioSesion.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -11,89 +11,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'TFg',
-      home: MyHomePage(),
+      title: 'TFG',
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 100.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "The Gourmet Shake",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 113, 185, 244),
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'PlayfairDisplay',
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 0.0),
                 child: Image.asset('assets/logo.jpg'),
               ),
-            ),
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyAppInicio()),
-                    );
-                  },
-                  child: Text('Inicio de Sesión',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 113, 185, 244),
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                    minimumSize: Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: 'Correo Electrónico',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Registro()),
-                    );
-                  },
-                  child: Text(
-                    'Registro',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(77, 198, 21, 0.047),
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
-                    minimumSize: Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
-              ],
-            )
-          ],
+                obscureText: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Obtener el contexto adecuado para la navegación
+                  final NavigatorState navigator = Navigator.of(context);
+                  // Navegar a la ventana "Inicio Sesion" cuando se presiona el botón
+                  navigator.push(MaterialPageRoute(builder: (context) => MyAppInicio()));
+                },
+                child: Text('Iniciar Sesión'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 113, 185, 244),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ), 
+            ],
+          ),
         ),
       ),
     );
